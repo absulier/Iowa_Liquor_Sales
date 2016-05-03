581a2eb14fad130023c60ca3f71c6cc6790538b9
Adam Sulier <absulier@gmail.com>
2016-05-02 22:06:18 -0400

    Tested more variables and built multidimensional regression

--- Lab3_3_4.py
+++ Lab3_3_4.py
@@ -14,22 +14,67 @@ del q115['Unnamed: 0']
 del s15['Unnamed: 0']
 
 lm = linear_model.LinearRegression()
-
 sales15= pd.merge(s15,q115, how='outer')
 sales15=sales15.dropna()
 
-x=sales15[u'sale_total_15q1'].reshape((len(sales15),1))
-y=sales15[u'sale_total_15'].reshape((len(sales15),1))
-
+x=sales15['sale_total_15q1']
+y=sales15['sale_total_15']
 plt.scatter(x, y)
 plt.xlabel("Total Sales 2015 Q1")
 plt.ylabel("Total Sales 2015")
 plt.show()
 
+x=sales15['sale_mean_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Mean Sales 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+x=sales15['price_per_liter_mean_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Mean Price Per Liter 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+x=sales15['vol_sol_l_sum_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Total Liters Sold 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+x=sales15['vol_sold_l_mean_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Mean Liters Sold 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+x=sales15['margin_mean_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Mean Margin 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+x=sales15['margin_sum_15q1']
+y=sales15['sale_total_15']
+plt.scatter(x, y)
+plt.xlabel("Total Margin 2015 Q1")
+plt.ylabel("Total Sales 2015")
+plt.show()
+
+
+x=sales15[['sale_total_15q1','vol_sol_l_sum_15q1','margin_sum_15q1']]
+
+lm = linear_model.LinearRegression()
 model = lm.fit(x, y)
 predictions = lm.predict(x)
-print lm.score(x,y)
-plt.scatter(y,predictions)
-plt.xlabel("True Values")
-plt.ylabel("Predicted Values")
-plt.show()
+print "Sample:", lm.score(x, y)
+
+lm = linear_model.RidgeCV()
+model = lm.fit(x, y)
+predictions = lm.predict(x)
+print "Sample with regularization:", lm.score(x, y)
